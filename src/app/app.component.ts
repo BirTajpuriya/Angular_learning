ed9ac3cc39b68d598d9e5f33f01e7d834f89b2dc
Bir Tajpuriya <tajpuriyabir00@gmail.comm>
2022-12-25 13:00:54 +0545

    validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import {FormGroup,FormControl,FormControlName,Validators} from '@angular/forms'
 export class AppComponent {
   title = 'reactive-form';
   loginform=new FormGroup({
-    username:new FormControl('',[Validators.required]),
+    username:new FormControl('',[Validators.required]), //this are validations
     email:new FormControl('',[Validators.required,Validators.minLength(5)])
   })
 
@@ -18,6 +18,8 @@ export class AppComponent {
     console.log(this.loginform.value);
   }
 
+
+  // to display error in span tag
   get username(){
     return this.loginform.get('username')
   }
@@ -27,50 +29,6 @@ export class AppComponent {
   }
 
 }
-
-
-
-// ********************reactive from guidelines*****************
-/*   
-1. firstly create a new project
-2. import ReactiveFormsModule in app.module.ts
-3.Go to,app.component.ts
-    & import FormGroup, FormControl,FormControlName
-4. Now make a FormGroup in app.component.ts file
-    Note: FormGroup is for controlling the whole form. It represents to form
-
-5. Inside FormGroup, in order to control the input field, we need to make a FormControl.
-
-//Example for No.4 & 5
- loginform=new FormGroup({       // No.4 ans & below all refers to no.5 ans
-  username:new FormControl(),
-  email:new FormControl(),
-  password:new FormControl()
- })
-
-
- Here inside the loginform FormGroup,- username,email & password are the
-name for input type field which is also known as FormControlName in Angular.
-
-=>We need to write FormControlName for each input field in order to access the 
-input value by the user.
-for example:
-
-Name:
-<input type="text" name="name" FormControlName="username">
-Email:
-<input type="email" name="email" FormControlName="email"/>
-Password:
-<input type="password" name="psw" FormControlName="password"/>
-
-after this all, go to html(template) file and 
-=> Inside <form> tag write [FormGroup]="anyname" // FormGroup is fixed but 
-in the place of anyname,you can write any name mention in .ts file.
-
-=> Generally we perform certain event on form data submit.
-In order to do so, we need to mention inside the <form> tag (ngSubmit)="method_name()"
-
-*/
 
 
 
