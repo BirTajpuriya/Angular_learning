90cd54e7ba24b427ec35499032445276e781ee04
Bir Tajpuriya <tajpuriyabir00@gmail.comm>
2022-12-25 11:55:04 +0545

    reactive form guidelines

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,3 +21,40 @@ export class AppComponent {
     
   }
 }
+
+
+
+// ********************reactive from guidelines*****************
+/*   
+1. firstly create a new project
+2. import ReactiveFormsModule in app.module.ts
+3.Go to,app.component.ts
+    & import FormGroup, FormControl,FormControlName
+4. Now make a FormGroup in app.component.ts file
+    Note: FormGroup is for controlling the whole form. It represents to form
+
+5. Inside FormGroup, in order to control the input field, we need to make a FormControl.
+
+//Example for No.4 & 5
+ loginform=new FormGroup({       // No.4 ans & below all refers to no.5 ans
+  username:new FormControl(),
+  email:new FormControl(),
+  password:new FormControl()
+ })
+
+
+ Here inside the loginform FormGroup,- username,email & password are the
+name for input type field which is also known as FormControlName in Angular.
+
+=>We need to write FormControlName for each input field in order to access the 
+input value by the user.
+for example:
+
+Name:
+<input type="text" name="name" FormControlName="username">
+Email:
+<input type="email" name="email" FormControlName="email"/>
+Password:
+<input type="password" name="psw" FormControlName="password"/>
+
+*/
