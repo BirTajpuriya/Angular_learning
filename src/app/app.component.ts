aa3602e7b1b6f8ab33cbecade034ddb1d3f8ecf7
Bir Tajpuriya <tajpuriyabir00@gmail.comm>
2022-12-25 11:37:18 +0545

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormGroup,FormControl,FormControlName} from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reactive-form';
+  loginform=new FormGroup({
+    username:new FormControl('bir'),
+    email:new FormControl('abc@gmail.com')
+  })
+
+  // function to display results
+  // input:any
+  show_result(){
+    // let input=this.loginform.value;
+    console.log(this.loginform.value);
+    
+  }
 }
