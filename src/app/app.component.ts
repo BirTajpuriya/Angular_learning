b26ec3d666999eb299b7152be3b37b9834799e1c
Bir Tajpuriya <tajpuriyabir00@gmail.comm>
2022-12-27 12:57:15 +0545

    two way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,34 +6,17 @@ import {FormGroup,FormControl,FormControlName,Validators} from '@angular/forms'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'reactive-form';
+  Myname:any='';
 }
-
-
-
-
-/* *****************************************Angular Directives************************
-
-=> Main objectives: to add additional behavior to elements.
-=>Directives are the classes that add additional behavior to elements in angular application
-=>Use angular built in directives to manage forms,lists,styles and what user see.
-
-=> whatever a user see, can be change or manipulated using directives.
-=>1.component is also directives that is used with template.
-
-=>2. Attribute directives
-=>like classes and styles
-=>changes the appearance and behavior of elements, components or another directives.
-=>
-*************************Built in attribute directives
-=>ngClass, ngModel, ngStyle
-
-
-=>3.Strucutural directives
-=>changing the DOM layout by adding and removing the DOM elements.
-Built in structural directives
-=>ngIf,ngFor,ngSwitch
-
+/* *********************Two way Binding****************************
+=> Defination: In the same time user able to see write and see the result.
+Steps:
+=> import FormsModule in app.module.ts
+=>in the input tag use [(ngModel)]="valueName"
+=>in ts file declare or define the value(valueName).
+=> in template, use interpolation {{valueName}} to display the input.
 
 */
