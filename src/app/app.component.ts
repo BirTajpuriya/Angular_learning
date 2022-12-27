d233c25a01c67fb65e9359bd4d5c53fd88932255
Bir Tajpuriya <tajpuriyabir00@gmail.comm>
2022-12-27 13:56:45 +0545

    *ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {FormGroup,FormControl,FormControlName,Validators} from '@angular/forms'
 
 @Component({
@@ -7,16 +7,52 @@ import {FormGroup,FormControl,FormControlName,Validators} from '@angular/forms'
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'reactive-form';
   Myname:any='';
-}
-/* *********************Two way Binding****************************
-=> Defination: In the same time user able to see write and see the result.
-Steps:
-=> import FormsModule in app.module.ts
-=>in the input tag use [(ngModel)]="valueName"
-=>in ts file declare or define the value(valueName).
-=> in template, use interpolation {{valueName}} to display the input.
 
+  name:string='Clean Code';
+  author:string='Robert C Martin';
+  src:string='https://source.unsplash.com/random/150x150/?nature';
+
+  name2:string='Pragmatic Programmer';
+  author2:string='David Thomas';
+  src2:string='https://source.unsplash.com/random/150x150/?user,computer,programming'
+
+  // 
+  // isDisabled:boolean=false;
+
+  //for *ngIf
+  //first make false & after click on button display
+  isShowing:boolean=false;
+  constructor(){}
+
+  ngOnInit(): void {
+      
+  }
+
+  /*
+  //isDisabled Button
+  handleClick(){
+    this.isDisabled=true;
+  }
 */
+
+  //two way binding for variable
+  myName:string='';
+
+  // *ngfor
+  // after button clicks display the books
+  toggleBooks(){
+    // this.isShowing=true;
+    // or can be wriiten following
+    this.isShowing=!this.isShowing;
+
+  /*
+  this.isShowing=!this.isShowing;
+
+  // this means if isShowing is true, then make it false
+  // or isShowing is false, then make it true
+  */ 
+  }
+}
