a43f909e9cd6db53a9df174932db04a6f5ff6d5f
Bir Tajpuriya <tajpuriyabir00@gmail.comm>
2022-12-27 15:41:32 +0545

    ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import {FormGroup,FormControl,FormControlName,Validators} from '@angular/forms'
 
+
+// defining the properties
+interface Book{
+  name:string;
+  author:string;
+  src:string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -10,14 +17,37 @@ import {FormGroup,FormControl,FormControlName,Validators} from '@angular/forms'
 export class AppComponent implements OnInit{
   title = 'reactive-form';
   Myname:any='';
+/*
+   name:string='Clean Code';
+   author:string='Robert C Martin';
+   src:string='https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/518z5dvykPL._SX348_BO1,204,203,200_.jpg';
 
-  name:string='Clean Code';
-  author:string='Robert C Martin';
-  src:string='https://source.unsplash.com/random/150x150/?nature';
+   name2:string='Pragmatic Programmer';
+   author2:string='David Thomas';
+   src2:string='https://source.unsplash.com/random/150x150/?user,computer,programming'
 
-  name2:string='Pragmatic Programmer';
-  author2:string='David Thomas';
-  src2:string='https://source.unsplash.com/random/150x150/?user,computer,programming'
+  */
+
+  /* We also can show the books by the use of *ngFor
+  => we first make a properties(object types) with key value pair.
+  =>look like json format
+  =>array of objec types
+
+  => After making array properties, define the properties using the interface
+  */
+
+  books:Book[]=[{
+  name:'Clean Code',
+  author:'Robert C Martin',
+  src:'https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/518z5dvykPL._SX348_BO1,204,203,200_.jpg'
+  },
+  {
+    name:'Pragmatic Programmer',
+    author:'David Thomas',
+    src:'https://source.unsplash.com/random/150x150/?user,computer,programming'
+  }
+]
+
 
   // 
   // isDisabled:boolean=false;
