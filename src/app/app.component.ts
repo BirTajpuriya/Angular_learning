1fc504054eeea32f8e79871789fc9842dd26ba9f
Bir Tajpuriya <tajpuriyabir00@gmail.comm>
2022-12-27 12:20:58 +0545

    angular directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -59,3 +59,32 @@ we need to make a specific function to check input field value.
 
  <span style="color:red" *ngIf="user && user.invalid && user.touched">The field is not valid</span>      //here user is the function name =>get user() 
 */ 
+
+
+
+
+
+/* *****************************************Angular Directives************************
+
+=> Main objectives: to add additional behavior to elements.
+=>Directives are the classes that add additional behavior to elements in angular application
+=>Use angular built in directives to manage forms,lists,styles and what user see.
+
+=> whatever a user see, can be change or manipulated using directives.
+=>1.component is also directives that is used with template.
+
+=>2. Attribute directives
+=>like classes and styles
+=>changes the appearance and behavior of elements, components or another directives.
+=>
+*************************Built in attribute directives
+=>ngClass, ngModel, ngStyle
+
+
+=>3.Strucutural directives
+=>changing the DOM layout by adding and removing the DOM elements.
+Built in structural directives
+=>ngIf,ngFor,ngSwitch
+
+
+*/
