9b67e3f964e659775d6d53dfd67d5d4f77e51036
Bir Tajpuriya <tajpuriyabir00@gmail.comm>
2022-12-29 12:11:41 +0545

    parent to child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {FormGroup,FormControl,FormControlName,Validators} from '@angular/forms'
 
 
 // defining the properties
-interface Book{
-  name:string;
-  author:string;
-  src:string;
-}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -15,76 +10,12 @@ interface Book{
 })
 
 export class AppComponent implements OnInit{
-  title = 'reactive-form';
-  Myname:any='';
-/*
-   name:string='Clean Code';
-   author:string='Robert C Martin';
-   src:string='https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/518z5dvykPL._SX348_BO1,204,203,200_.jpg';
-
-   name2:string='Pragmatic Programmer';
-   author2:string='David Thomas';
-   src2:string='https://source.unsplash.com/random/150x150/?user,computer,programming'
-
-  */
-
-  /* We also can show the books by the use of *ngFor
-  => we first make a properties(object types) with key value pair.
-  =>look like json format
-  =>array of objec types
-
-  => After making array properties, define the properties using the interface
-  */
-
-  books:Book[]=[{
-  name:'Clean Code',
-  author:'Robert C Martin',
-  src:'https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/518z5dvykPL._SX348_BO1,204,203,200_.jpg'
-  },
-  {
-    name:'Pragmatic Programmer',
-    author:'David Thomas',
-    src:'https://source.unsplash.com/random/150x150/?user,computer,programming'
-  }
-]
-
-
-  // 
-  // isDisabled:boolean=false;
-
-  //for *ngIf
-  //first make false & after click on button display
-  isShowing:boolean=false;
-  constructor(){}
-
+  constructor() { }
   ngOnInit(): void {
-      
   }
-
-  /*
-  //isDisabled Button
-  handleClick(){
-    this.isDisabled=true;
-  }
-*/
-
-  //two way binding for variable
-  myName:string='';
-
-  // *ngfor
-  // after button clicks display the books
-  toggleBooks(){
-    // this.isShowing=true;
-    // or can be wriiten following
-    this.isShowing=!this.isShowing;
-
-  /*
-  this.isShowing=!this.isShowing;
-
-  // this means if isShowing is true, then make it false
-  // or isShowing is false, then make it true
-  */ 
-  }
+  title = 'reactive-form';
+  valueTopass="Hello from parent";
+  
 }
 
 /*
--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -6,5 +6,14 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./child.component.css']
 })
 export class ChildComponent {
-@Input() placeHolder:string='Enter your name';
+  @Input() myData:string='';
+  // above code means
+  //jaha hami input decorator used garxeu, teha tesle value parent component ma khojxa ra value receive garxa
+  //we have used Input decorator to get the data from parent component
+  //myData is the variable which is used to store the data from parent component
+  //we have used string type because we are passing string type data from parent component
+
+
+  //aba hamile child component ma data used garna sakxeu.
+
 }
